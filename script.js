0b1470f0fb262d1852ab16333686ff3ba049df90
Spreeha Dutta <spreehadutta@gmail.com>
2020-08-15 12:20:33 +0530

    added unchecked count

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
-//const list = document.getElementById('todo-list')
-var itemCountSpan = document.getElementById('item-count')
-var uncheckedCountSpan = document.getElementById('unchecked-count')
+const element = document.getElementById('todo-list')
+const itemCountSpan = document.getElementById('item-count')
+const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
   /*alert('New TODO button clicked!')*/
@@ -15,80 +15,32 @@ function newTodo() {
   var list = document.createElement("li");
   var label = document.createTextNode(task);
 
-  /*var check = document.createElement("input");
+  var check = document.createElement("input");
   check.setAttribute("type", "checkbox");
-  check.setAttribute("id", "myCheck");*/
+  check.setAttribute("id", "myCheck");
   //check.setAttribute("onclick","myFunction()")
   if(task!=null)
   {
-    list.innerHTML = `<input type="checkbox" onclick="myFunction()" class= 'myCheck'>`;
-    //list.appendChild(check);
+    list.appendChild(check);
     list.appendChild(label);
 
-    var element = document.getElementById("todo-list");
     element.appendChild(list);
 
     var items = Number(itemCountSpan.innerHTML)+1;
     itemCountSpan.innerHTML=items
   }
 
-  var count = 0;
-  var uncheck = items - count;
-  uncheckedCountSpan.innerHTML=uncheck
+  let count=0;
+  var uncheck = Number(uncheckedCountSpan.innerHTML)
+  uncheckedCountSpan.innerHTML=uncheck+1
 
-  function myFunction(){
-    alert('hello!')
-    var myChecklist = document.getElementsById("myCheck");
-    var i;
-    for (i = 0; i < myChecklist.length; i++) {
-      if(myChecklist[i].checked)
-        {
-            count = count + 1;
-            var uncheck = items - count;
-            alert(uncheck)
-            uncheckedCountSpan.innerHTML=uncheck
+  check.addEventListener("click", function() {
+    uncheck = Number(uncheckedCountSpan.innerHTML)
+    if(this.checked)
+      uncheck = uncheck - 1;
+    else
+      uncheck = uncheck + 1;
+    uncheckedCountSpan.innerHTML=uncheck
+  });
 
-        }
-    }
-    alert('hello!')
-  };
-
-  /*
-  function myFunction() {
-    var myChecklist = document.getElementsById("myCheck");
-    var i;
-    for (i = 0; i < myChecklist.length; i++) {
-      if(myChecklist[i].type == checkbox && myChecklist[i].checked === true)
-        {
-            count = count + 1;
-            var uncheck = items - count;
-            alert(uncheck)
-            uncheckedCountSpan.innerHTML=uncheck
-
-        }
-    }
-    alert('hello!')
-  }
-  */
-
-    /*
-    var myChecklist = document.getElementsByTagName("INPUT");
-    var i;
-    for (i = 0; i < myChecklist.length; i++) {
-      if(myChecklist[i].type == checkbox && myChecklist[i].checked === true)
-        {
-            count = count + 1;
-            var uncheck = items - count;
-            alert(uncheck)
-            uncheckedCountSpan.innerHTML=uncheck
-
-        }
-    }
-    */
-
-
-
-
-  /*document.write('Number of total items '+items)
-  console.log('Number of unchecked items '+uncheckedCountSpan)*/
 }
